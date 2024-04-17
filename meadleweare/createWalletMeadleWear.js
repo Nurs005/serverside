54e2dbba586a8188a554b3cac36c5c772b803918
Nurs005 <tenb1105@gmail.com>
2024-04-18 00:42:52 +0600

    to newww rpc

--- meadleweare/createWalletMeadleWear.js
+++ meadleweare/createWalletMeadleWear.js
@@ -1,6 +1,6 @@
 const { Web3 } = require('web3');
 const { create } = require('../models/User');
-const endpont = 'https://eth-sepolia.g.alchemy.com/v2/EIZEUwfzOQuoGs2Fcjau20VC5dC7-7rl'
+const endpont = 'https://eth-mainnet.g.alchemy.com/v2/nsy1wVdd1DkxbDrnnC26FfzKhWmI4Lhn'
 const web3 = new Web3(endpont)
 
 const createWallet = async (req, res, next) => {
