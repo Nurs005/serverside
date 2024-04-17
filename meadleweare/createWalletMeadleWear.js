b2574943e987c777c77d71082f7d52cde6d0bf8c
Nurs005 <tenb1105@gmail.com>
2024-04-18 03:08:19 +0600

    new wallet create logic

--- meadleweare/createWalletMeadleWear.js
+++ meadleweare/createWalletMeadleWear.js
@@ -3,10 +3,25 @@ const { create } = require('../models/User');
 const endpont = 'https://eth-sepolia.g.alchemy.com/v2/AIepbTs1NizYh3mlVMsgVXVzIYPMmVPk'
 const web3 = new Web3(endpont)
 
+
+let lastCreationTime = 0;
+const creationDelay = 30000;
+
 const createWallet = async (req, res, next) => {
-    const account = web3.eth.accounts.wallet.create(1)
-    req.account = account
-    next()
-}
+    try {
+        const currentTime = Date.now();
+        if (currentTime - lastCreationTime < creationDelay) {
+            return res.status(400).json('Слишком частое создание кошельков');
+        }
+        const account = web3.eth.accounts.wallet.create(1);
+        req.account = account;
+        lastCreationTime = currentTime;
+
+        next();
+    } catch (error) {
+        console.error('Произошла ошибка при создании кошелька:', error);
+        res.status(500).json({ error: 'Ошибка при создании кошелька' });
+    }
+};
 
 module.exports = createWallet
