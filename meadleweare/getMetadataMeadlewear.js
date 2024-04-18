2873d470799c1912a7eaf80cf10e9eff1cbe497a
Nurs005 <tenb1105@gmail.com>
2024-04-18 06:23:23 +0600

    new fetch meta

--- meadleweare/getMetadataMeadlewear.js
+++ meadleweare/getMetadataMeadlewear.js
@@ -7,12 +7,14 @@ const getDataFromMetadata = async (req, res) => {
         }
 
         const metadataArray = req.metadata;
-        const metadataPromises = metadataArray.map(async (metadataUrl) => {
+        const metadataPromises = metadataArray.map(async (metadataUrl, index) => {
             const response = await fetch(metadataUrl);
             if (!response.ok) {
                 throw new Error(`Ошибка при запросе к ${metadataUrl}`);
             }
-            return response.json();
+            const metadata = await response.json();
+            // Добавляем свойство "id" и обертываем в новый объект с ключом "metadata"
+            return { id: index + 1, metadata };
         });
 
         const metadataResults = await Promise.all(metadataPromises);
@@ -21,6 +23,7 @@ const getDataFromMetadata = async (req, res) => {
         return res.status(400).json(error.message);
     }
 }
+
 
 
 module.exports = getDataFromMetadata;
